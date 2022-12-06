cb566ca6765476ffc106d256ef1735b51f2d9c89
iskhakabdukhamitov <abdulhamitov.bstn@gmail.com>
2022-12-06 15:25:01 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Set<Integer> set1 = new LinkedHashSet<>(List.of(1,2,3));
+        Set<Integer> set2 = new LinkedHashSet<>(List.of(0,1,2));
+        System.out.println(symmetricDifference(set1, set2));
+    }
+
+    public static Set<Integer> symmetricDifference (Set<Integer> set1, Set<Integer> set2){
+        Set<Integer> contain = new TreeSet<>(set1);
+        contain.addAll(set2);
+        Set<Integer> sorted = new TreeSet<>(set1);
+        sorted.retainAll(set2);
+        contain.removeAll(sorted);
+        return contain;
+    }
+}
